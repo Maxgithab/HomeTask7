187975b6eef933cf2f8128423b06f5cb176379a9
GitVerseVerse <m.a-volkov@mail.ru>
2026-02-09 12:27:33 +0700

    HomeWork 14, Task 1.1

--- src/main/java/org/example/HomeWork14/Task1/LongestFile.java
+++ src/main/java/org/example/HomeWork14/Task1/LongestFile.java
@@ -22,15 +22,18 @@ public class LongestFile {
             inputFile.close();
 
             String[] words = text.split("[^a-zA-Z']+");
+
             String longestWord = "";
 
             for (String word : words) {
-                if (word.length() > longestWord.length()) {
+                if (word.length() > longestWord.length()){
                     longestWord = word;
+              //  } else if (word.length() == longestWord.length() && !word.equals(longestWord)) {
+               //     longestWord += ", " + word;
                 }
             }
 
-            System.out.println("Самое длинное слово: " + longestWord);
+            System.out.println("Самое длинное слово: " + longestWord + "\nДлина: " + longestWord.length());
 
             Path outputPath = Paths.get("longest.txt");
             Files.writeString(outputPath, longestWord);
--- src/main/java/org/example/HomeWork14/Task1/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example.HomeWork14.Task1;
-
-public class Main {
-    public static void main(String[] args) {
-        LongestFile searchWord = new LongestFile();
-
-    }
-
-}
